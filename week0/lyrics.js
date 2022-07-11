54373d09aa82835a97ae79a48704317006d4549b
HUST_LY <1596087252@qq.com>
2022-07-11 23:09:40 +0800

    基本实现了播放器

--- week0/lyrics.js
+++ week0/lyrics.js
@@ -1,10 +1,13 @@
+let isScroll=false;
+
 //处理歌词的获取，同步等问题
 
 //每句歌词可以解析为时间和歌词内容，创建一个歌词类
 class lyrics_model {
-    constructor(time,lyrics) {
+    constructor(time,jp_lyrics,ch_lyrics) {
         this.time=time;
-        this.lyrics=lyrics;
+        this.jp_lyrics=jp_lyrics;
+        this.ch_lyrics=ch_lyrics;
     }
 }
 //存储每句歌词的数组
@@ -21,48 +24,104 @@ function add_obj(sentence){
 //将歌词渲染到页面上
 function list_render() {
     for(let i in list) {
-        let lyrics_content=document.createElement("p");
-        lyrics_content.className="lyrics_p"
-        lyrics_content.innerText=list[i].lyrics;
+        let lyrics_content=document.createElement("div");
+        lyrics_content.className="lyrics_content";
+        let jp_p=document.createElement("p");
+        jp_p.className="jp_lyrics";
+        let ch_p=document.createElement("p");
+        jp_p.innerText=list[i].jp_lyrics;
+        if(list[i].ch_lyrics) {
+            ch_p.innerText=list[i].ch_lyrics;
+            ch_p.className="ch_lyrics"
+        }
+        lyrics_content.appendChild(jp_p).appendChild(ch_p);
         lyrics_list.appendChild(lyrics_content);
     }
 }
-
-//控制歌词同步
-function center_lyrics(list) {
-    console.log("ok")
-    for(let i in list) {
-        let p=document.querySelectorAll(".lyrics_p");
-        if(list[i].time===audio.currentTime) {
-           p[i].style.fontSize="50px";
-        }else {
-            p[i].style.fontSize="16px";
+//节流
+function throttle(callback, wait) {
+    // 上一次点击的时间
+    let lastTime = 0;
+    return function () {    // 如果不return的话会立即执行，return的话就是点击才会调用此方法
+        // 获取当前点击的时间
+        let nowTime = new Date().getTime()
+        if (nowTime - lastTime > wait) {
+            callback();
+            // 把当前点击的时候  赋值给上一次的时间
+            lastTime = nowTime;
         }
     }
 }
+function scroll_change(){
+    isScroll=true;
+    setTimeout(()=>{
+        isScroll=false
+    },10000)
+}
+
 //fetch获取歌词文件
 fetch("./lyrics.txt").then(res=>{
-    return res.text();
+    return res.text();//获取日文歌词
 }).then(data=>{
-    let each_sentence=data.toString().split("\\n");
-    each_sentence.pop();//去除由末尾换行符造成的空字符串
-    for(let i in each_sentence){
-        let time=add_obj(each_sentence[i]);
-        let jp_lyrics=each_sentence[i].split("]")[1];
-        list.push(new lyrics_model(time,jp_lyrics));
-    }
-    return list;//下面将对歌曲list进行操作
-}).then(list=>{
-    list_render(list);
-    audio.addEventListener("timeupdate",()=>{
-        for(let i in list) {
-            let p=document.querySelectorAll(".lyrics_p");
-            if(parseFloat(list[i].time)<parseFloat(audio.currentTime)) {
-                if(i>=1) {
-                    p[i-1].style.fontSize="16px";
-                }
-                p[i].style.fontSize="50px";
+    fetch("./lyrics_trans.txt").then(res_1=>{
+        return res_1.text();//获取中文歌词
+    }).then(data_1=>{
+        let ch_data=data_1.split("\\n");
+        let each_sentence=data.toString().split("\\n");
+        each_sentence.pop();//去除由末尾换行符造成的空字符串
+        for(let i in each_sentence){
+            let time=add_obj(each_sentence[i]);
+            if(i>=3){
+                let jp_lyrics=each_sentence[i].split("]")[1];
+                let ch_lyrics=ch_data[i-3].split("]")[1];
+                list.push(new lyrics_model(time,jp_lyrics,ch_lyrics));
+            }
+            else {
+                let jp_lyrics=each_sentence[i].split("]")[1];
+                list.push(new lyrics_model(time,jp_lyrics))
             }
         }
+        return list
+    }).then(list=>{
+        list_render(list);
+    }).then(()=>{
+        let lrc_content=document.querySelectorAll(".lyrics_content");
+        for(let i in lrc_content) {
+            lrc_content[i].onclick=()=>{
+                audio.currentTime=list[i].time;//点击歌词跳转播放
+                for(let j=0;j<lrc_content.length;j++) {
+                    if(j!==i) {
+                        lrc_content[j].style.fontWeight="normal"
+                    }
+                }//让之前加粗的歌词不再加粗
+
+            }
+        }
+        audio.addEventListener("timeupdate",()=>{
+            for(let i=0;i<list.length;i++) {
+                if(i>0) {
+                    if(list[i].time>audio.currentTime&&list[i-1].time<audio.currentTime) {
+                        lyrics_list.addEventListener("mousewheel",throttle(scroll_change,1000))
+                        if(!isScroll) {
+                            lyrics_list.scrollTo({
+                                top:lrc_content[i].offsetTop-0.5*document.documentElement.clientHeight,
+                                behavior:"smooth"
+                            })
+                        }
+                        lrc_content[i-1].style.fontWeight="bold";//播放到的歌词加粗
+
+                        if(i>1) {
+                            lrc_content[i-2].style.fontWeight="normal";
+                        }
+                    }
+                    else if(i===list.length-1&&list[i].time<audio.currentTime) {
+                        //播放到最后一句
+                        lrc_content[i].style.fontWeight="bold"
+                        lrc_content[i-1].style.fontWeight="normal";
+                    }
+                }
+            }
+        })
     })
+
 })
--- week0/main.js
+++ week0/main.js
@@ -1,1 +1,80 @@
+let play_change=false;
+let night_mode=false;
+//转换歌曲时间的函数
+function time_change(time) {
+    let time_num=parseFloat(time);
+    let min=Math.floor(time_num/60);
+    let sec=Math.floor(time_num-min*60);
+    if(sec<10) {
+        sec="0"+sec.toString();
+    }
+    else {
+        sec=sec.toString()
+    }
+    return "0"+min.toString()+":"+sec;
+}
 
+audio.addEventListener("canplay",()=>{
+    time_info.innerHTML=time_change(audio.currentTime)+"/"+time_change(audio.duration)
+})
+audio.addEventListener("pause",()=>{
+    play_button.innerHTML="<svg t=\"1657520876829\" class=\"icon\" viewBox=\"0 0 1024 1024\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" p-id=\"4132\" width=\"200\" height=\"200\"><path d=\"M224 938.713333a53.58 53.58 0 0 1-53.333333-53.433333V138.72a53.333333 53.333333 0 0 1 80.886666-45.666667l618.666667 373.28a53.333333 53.333333 0 0 1 0 91.333334l-618.666667 373.28a53.16 53.16 0 0 1-27.553333 7.766666z m0.046667-810.666666a10.98 10.98 0 0 0-5.333334 1.42 10.466667 10.466667 0 0 0-5.38 9.253333v746.56a10.666667 10.666667 0 0 0 16.18 9.133333l618.666667-373.28a10.666667 10.666667 0 0 0 0-18.266666l-618.666667-373.28a10.386667 10.386667 0 0 0-5.446666-1.586667z\" fill=\"#ffffff\" p-id=\"4133\"></path></svg>"
+})
+audio.addEventListener("play",()=>{
+    play_button.innerHTML="<svg t=\"1657521279735\" class=\"icon\" viewBox=\"0 0 1024 1024\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" p-id=\"5117\" width=\"200\" height=\"200\"><path d=\"M247.731946 0a49.546389 49.546389 0 0 0-49.546389 49.546389v924.907222a49.546389 49.546389 0 0 0 99.092779 0V49.546389a49.546389 49.546389 0 0 0-49.54639-49.546389zM776.268054 0a49.546389 49.546389 0 0 0-49.54639 49.546389v924.907222a49.546389 49.546389 0 0 0 99.092779 0V49.546389a49.546389 49.546389 0 0 0-49.546389-49.546389z\" p-id=\"5118\" fill=\"#ffffff\"></path></svg>"
+})
+play_button.addEventListener("click",()=>{
+    if(play_change) {
+        audio.pause();
+        play_change=false;
+        record.style.animationPlayState="paused"
+    }else {
+        audio.play();
+        play_change=true;
+        record.style.animationPlayState="running"
+    }
+})
+audio.addEventListener("timeupdate",()=>{
+    time_info.innerHTML=time_change(audio.currentTime)+"/"+time_change(audio.duration);
+    over_loading.style.width=document.documentElement.clientWidth*(audio.currentTime/audio.duration)+"px";
+})
+audio.addEventListener("ended",()=>{
+    audio.play();
+    play_change=true;
+    record.style.animationPlayState="running"
+})
+loading_ball.addEventListener("click",(e)=>{
+    let x=e.clientX;
+    audio.currentTime=(x/document.documentElement.clientWidth)*audio.duration;
+})
+change_model.addEventListener("click",()=>{
+    if(night_mode) {
+        body.style.background="#f0f0f0";
+        body.style.color="black";
+        loading_ball.style.background="white";
+        record.style.borderColor="darkgray"
+        night_mode=false;
+    }
+    else {
+        body.style.background="#575757";
+        body.style.color="#b2b2b2";
+        loading_ball.style.background="gray";
+        record.style.borderColor="#343434 "
+        night_mode=true;
+    }
+
+
+})
+ball.onmousedown=()=>{
+    document.onmousemove=(e)=>{
+        let x=e.clientX;
+        over_loading.style.width=x+"px";
+    }
+    document.onmouseup = (e) => {
+        let x=e.clientX;
+        audio.currentTime=(x/document.documentElement.clientWidth)*audio.duration;
+        document.onmousemove = null;
+        document.onmouseup = null;
+        document.onmousedown = null;
+    }
+}
